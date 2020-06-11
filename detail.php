81caf1122f9b0b3a4d891f7d271cdd475aba20d0
rafael defelice <defelicerafael@gmail.com>
2020-06-11 00:58:10 -0300

    v1.1
    Pagar la compra

--- detail.php
+++ detail.php
@@ -192,7 +192,13 @@ $preference->save();
                                             <?php echo  $_POST['unit'] ."U. Usuario de Prueba"; ?>
                                         </h3>
                                     </div>
-                                    <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
+                                    <form action="/procesar-pago" method="POST">
+                                        <script
+                                         src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+                                         data-preference-id="<?php echo $preference->id; ?>"
+                                         data-button-label="Pagar la compra">
+                                        </script>
+                                    </form>
                                     <!--<button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>-->
                                 </div>
                             </div>
